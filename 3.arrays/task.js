372b569c9353c31c6da1da39c1dce3de6f153306
Alex <jswift888@gmail.com>
2023-07-31 11:10:37 +0300

    task 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    return arr1.every(function(element, index) {
+        return element === arr2[index];
+    });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users
+        .filter(user => user.gender === gender)
+        .map(user => user.age)
+        .reduce((sum, age, index, array) => {
+            sum += age;
+            if (index === array.length - 1) {
+                return sum / array.length;
+            } else {
+                return sum;
+            }
+        }, 0);
 }
